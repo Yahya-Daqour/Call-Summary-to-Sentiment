daa2b9a8f4701396340625ddbbf90199d08e6eff
Yahya-Daqour <73020453+Yahya-Daqour@users.noreply.github.com>
2025-08-07 17:21:00 +0300

    Add LLM class

--- src/backend/model.py
+++ src/backend/model.py
@@ -16,3 +16,62 @@ class SentimentAnalyzer:
         scores = np.exp(scores) / np.sum(np.exp(scores)) 
         predicted_class = np.argmax(scores)
         return self.labels[predicted_class]
+
+class LLMSentimentAnalyzer:
+  def __init__(self, model_name="google/gemma-2b-it"):
+    self.device = "cuda" if torch.cuda.is_available() else "cpu"
+    self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+    # Load the model in 8-bit to reduce memory usage
+    self.model = AutoModelForCausalLM.from_pretrained(model_name).to(self.device)
+    self.labels = ["Negative", "Neutral", "Positive"]
+
+  def get_prompt(self, text: str) -> str:
+      prompt = f"""
+      You are a sentiment analysis assistant for customer service calls. Your task is to classify the sentiment of the given call summary in Arabic or English.
+
+      Please consider the tone, language, and content of the summary when making your classification. You must choose one of the following:
+      - Positive
+      - Neutral
+      - Negative
+
+      Here are some examples to make your classification:
+
+      Example 1:
+      Text: The customer was experiencing an issue with the laptop that has not been resolved yet after several attempts, and an appointment was scheduled to follow up on the case next Sunday.
+      Sentiment: Negative
+
+      Example 2:
+      Text: تواصل العميل مع مركز خدمة العمالء للحصول على شهادة رصيد حسابها. تم توجيهها إلى الموقع اإللكتروني للبنك إلصدار الشهادة وتوجيهها إلى جهة معينة
+      Sentiment: Neutral
+
+      Example 3:
+      Text: كان العميل يواجه صعوبة في سماع الوكيل أثناء المكالمة بسبب انخفاض مستوى الصوت. وافق الوكيل على إرسال رسالة نصية عبر تطبيق واتساب وأبدى العميل امتنانه بينما ينتظر اتصاالً معاوداً
+      Sentiment: Positive
+      Your classification should be based solely on the content of the call summary. Do not make any assumptions or inferences beyond what is explicitly stated.
+
+      Classify the sentiment of the following call summary:
+
+      {text}
+
+      Answer: """
+
+      return prompt
+
+  def extract_sentiment(self, output: str) -> str:
+    lines = output.splitlines()
+    last_line = lines[-1]
+    for label in self.labels:
+      if label in last_line:
+        return label
+    return "Unknown"
+
+  def predict(self, text: str) -> str:
+    prompt = self.get_prompt(text)
+    input_ids = self.tokenizer(prompt, return_tensors="pt").to(self.device)
+    with torch.no_grad():
+      outputs = self.model.generate(**input_ids)
+    decoded_output = self.tokenizer.decode(outputs[0])
+    
+    sentiment_answer = self.extract_sentiment(decoded_output)
+    
+    return sentiment_answer
